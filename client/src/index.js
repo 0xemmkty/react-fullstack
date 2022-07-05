5ae1a9d292b890d97543fbf303f577cbb407bbfe
EKMM <2733224120@qq.com>
2022-07-05 17:45:48 -0500

    reducers folder

--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
+);
