5614bd727647e50542c5e0be8195ec8f18881bbe
EKMM <2733224120@qq.com>
2022-07-20 13:23:40 -0500

    add link in Dashboard \.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,15 @@
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   );
